7bb16989db43282de7f0f5ab31ddc47bc17ad474
kauameloo <kaua.rodrigues2620@gmail.com>
2024-09-17 15:56:24 -0300

    Remove src/Utils folder from tracking

--- ativements/src/Utils/githubkey.js
+++
@@ -1,8 +0,0 @@
-import { Octokit } from "octokit";
-
-const token = "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";
-
-// Gerar a conexão com a api do github
-export const octokit = new Octokit({
-  auth: token,
-});
